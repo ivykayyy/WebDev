7665ed75e798b444a2446980cfce4753cad467f6
Ivy Kay M. Gaddao <60841709+ivykayyy@users.noreply.github.com>
2020-12-07 20:52:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
-
-include_once("ivy.html")
-include_once("products.html")
-include_once("about.html")
-include_once("contact.html")
-include_once("account.html")
-
-    
+include_once("ivy.html")    
 ?>
